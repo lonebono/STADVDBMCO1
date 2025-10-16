f590d7e905888550f5bc5160c81c4f48f7c247b8
lonebono <vaughnsy15@gmail.com>
2025-10-16 15:11:30 +0800

    fixed the import to local postgres

--- etl/load_raw.sql
+++ etl/load_raw.sql
@@ -1,6 +1,6 @@
 -- IMDb RAW SOURCE IMPORTS
 
 -- 1. title.akas
-\COPY title_akas FROM 'data/title.akas.tsv' (FORMAT csv, DELIMITER E'\t', HEADER true, NULL '\N');
+\COPY title_akas FROM 'data/title.akas.tsv' (FORMAT text, DELIMITER E'\t', NULL '\N', ENCODING 'UTF8');
 
 -- 2. title.basics
