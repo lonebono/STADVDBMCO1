3e7c772b17c77262a2274573f086df54d74179fb
lonebono <vaughnsy15@gmail.com>
2025-10-19 01:29:36 +0800

    created queries to import local pgadmin

--- etl/load_raw.sql
+++ etl/load_raw.sql
@@ -1,6 +1,10 @@
 -- IMDb RAW SOURCE IMPORTS
 
 -- 1. title.akas
-\COPY title_akas FROM 'data/title.akas.tsv' (FORMAT text, DELIMITER E'\t', NULL '\N', ENCODING 'UTF8');
+\COPY title_akas(titleid, ordering, title, region, language, types, attributes, isoriginaltitle) FROM 'data/title.akas.tsv' WITH (FORMAT text, DELIMITER E'\t', NULL '\N', ENCODING 'UTF8');
 
 -- 2. title.basics
+\COPY title_basics(tconst, titleType, primaryTitle, originalTitle, isAdult, startYear, endYear, runtimeMinutes, genres) FROM 'data/title.basics.tsv' WITH (FORMAT text, DELIMITER E'\t', NULL '\N', ENCODING 'UTF8');
+
+-- 3. title.ratings
+\COPY title_ratings(tconst, averageRating, numVotes) FROM 'data/title.ratings.tsv' WITH (FORMAT text, DELIMITER E'\t', NULL '\N', ENCODING 'UTF8');
